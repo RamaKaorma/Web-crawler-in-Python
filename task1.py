0d185d2e989b0da28d5c907b87bc0535947f390b
RamaKaorma <rkaorma@student.unimelb.edu.au>
2023-04-15 09:09:24 +1000

    Compared pages against robots.txt with sample links

--- task1.py
+++ task1.py
@@ -23,5 +23,21 @@ def task1(starting_links: List[str], json_filename: str) -> Dict[str, List[str]]
     # link as the key and the list of crawled links for the
     # value.
     # Implement Task 1 here
+    
+    # Specify the location of robots.txt
+    base_url = "http://115.146.93.142/samplewiki"
+    robots_item = '/robots.txt'
+
+    robots_url = base_url + robots_item
+    page = requests.get(robots_url)
+
+    from robots import process_robots, check_link_ok
+
+    robots_rules = process_robots(page.text)
+
+    test_links = ["http://115.146.93.142/wiki/Musical_tuning"]
+
+    for test_link in test_links:
+        print("Can we visit {}? {}".format(test_link, "Yes" if check_link_ok(robots_rules, test_link) else"No"))
 
     return {}
