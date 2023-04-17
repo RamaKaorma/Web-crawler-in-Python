c1a2e365c32c944060b6b1c63b1055235e24577b
RamaKaorma <rkaorma@student.unimelb.edu.au>
2023-04-17 15:16:25 +1000

    Tried implementing a set

--- task1.py
+++ task1.py
@@ -13,7 +13,7 @@ import urllib
 from robots import process_robots, check_link_ok
 
 # A simple page limit used to catch procedural errors.
-SAFE_PAGE_LIMIT = 1000
+SAFE_PAGE_LIMIT = 50
 
 
 # Task 1 - Get All Links (3 marks)
@@ -57,7 +57,6 @@ def task1(starting_links: List[str], json_filename: str) -> Dict[str, List[str]]
         
         # Initiate a dictionary to hold all the links and whether they've been visited
         visited = {}
-        output = {}
         visited[seed_url] = True
         pages_visited = 1
 
@@ -67,23 +66,17 @@ def task1(starting_links: List[str], json_filename: str) -> Dict[str, List[str]]
         # Search for all links that lead to "/sample_wiki" or so, storing them as the seed link
         path_segment = path.split('/')
         directory = base_url + '/' + path_segment[1]
-        seed_links = soup.findAll('a', href=re.compile("^{}".format(path)))
+        seed_links = soup.findAll('a', href=re.compile(f"^{path}"))
 
         # Check if the seed links cannot be visited, remove if so
+        to_visit = []
         for link in seed_links:
             sub_url = urlparse(link['href'])
             sub_path = sub_url.path
             if not check_link_ok(robot_rules, sub_path):
-                seed_links.remove(link)
-
-        # Check if the link 
-        to_visit = []
-        for link in links:
-            if link not in seed_links and "href" in link.attrs:
-                if not check_link_ok(robot_rules, link['href']):
-                    continue
-                to_visit.append(urljoin(seed_url, link['href']))
-
+                continue
+            to_visit.append(urljoin(seed_url, link['href']))
+            print(urljoin(seed_url, link['href']))
         # Find all outbound links on successor pages and explore each one
         while (to_visit):
             # Impose a limit to avoid breaking the site
@@ -94,7 +87,7 @@ def task1(starting_links: List[str], json_filename: str) -> Dict[str, List[str]]
             # Assign the first item from to_visit to link, and remove it from the to_visit list
                 # to make sure each link is visited once
             link = to_visit.pop(0)
-            # print(link)
+            print(link)
             # Get the webpage
             page = requests.get(link)
             if not page.ok:
@@ -105,10 +98,9 @@ def task1(starting_links: List[str], json_filename: str) -> Dict[str, List[str]]
             # Mark the item as visited, i.e., add to visited dict, remove from to_visit
             visited[link] = True
             new_links = soup.findAll('a')
-            
             for new_link in new_links:
                 # Skip the links that don't have href values (links that don't actually exist or don't lead anywhere)
-                if "href" not in new_link.attrs or path_segment[1] not in new_link:
+                if "href" not in new_link.attrs:
                     continue
 
                 new_item = new_link['href']
@@ -125,8 +117,8 @@ def task1(starting_links: List[str], json_filename: str) -> Dict[str, List[str]]
                 new_url = urljoin(link, new_item)
                 new_parsed_url = urlparse(new_url)
 
-                # Check it's not already in the list before adding it.
-                if (new_url not in visited) and (new_url not in to_visit) and (new_parsed_url.netloc == base_url):
+                # Check it's not already in the list before adding it  and (new_parsed_url.netloc == base_url).
+                if (new_url not in visited) and (new_url not in to_visit):
                     to_visit.append(new_url)
                 
             # Increase the number of pages we've visited so the page limit is enforced.
@@ -134,7 +126,7 @@ def task1(starting_links: List[str], json_filename: str) -> Dict[str, List[str]]
             
             # print("pages_visited = " + str(pages_visited))
             # print("to_visit = " + str(len(to_visit)))
-            # print("visited = " + str(len(visited)))
+            print("visited = " + str(len(visited)))
             # print()
             # print()
 
