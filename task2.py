402db980868b9f8ba8d796cc9499b8f142349c9c
RamaKaorma <rkaorma@student.unimelb.edu.au>
2023-04-19 15:04:35 +1000

    Done with first half of task 2

--- task2.py
+++ task2.py
@@ -15,8 +15,6 @@ from nltk.stem.porter import PorterStemmer
 
 from robots import process_robots, check_link_ok
 
-INVALID = {'th':['class', 'infobox-label'], 'div':['class', 'printfooter'], 'div':['id', 'toc'], 
-           'table':['class', 'ambox'], 'div':['class', 'asbox'], 'span':['class', 'mw-editsection']}
 
 # Task 2 - Extracting Words from a Page (4 Marks)
 def task2(link_to_extract: str, json_filename: str):
@@ -26,26 +24,39 @@ def task2(link_to_extract: str, json_filename: str):
     # is the link_to_extract, and its value is the 
     # list of words produced by the processing.
     # Implement Task 2 here
-    # print('\n\n' + link_to_extract + '\n\n')
-    page = requests.get(link_to_extract);
+    page = requests.get(link_to_extract)
     if not page.ok: # Skip if page is inaccessible
         return
 
     # Scrape the code, aka, get the html to pull out the links in the page, once done, flag as visited
     soup = bs4.BeautifulSoup(page.text, 'html.parser')
     body = soup.find('div', id='mw-content-text')
-    print(soup)
-    for tag in soup('th'):
-        if tag['class'] == 'infobox-label':
-            tag.decompose()
-    for tag in soup('div'):
-        if tag['class'] == ('printffoter' or 'asbox') or tag['id'] == 'toc':
-            tag.decompose()
-    for tag in soup('table'):
-        if tag['class'] == 'ambox':
-            tag.decompose()
-    for tag in soup('span'):
-        if tag['class'] == 'mw-editsection':
-            tag.decompose()
-    print(soup)
+
+    th = body.findAll('th', class_='infobox-label')
+    for tag in th:
+        tag.decompose()
+
+    div_footer = body.findAll('div', class_='printfooter')
+    for tag in div_footer:
+        tag.decompose()
+
+    div_toc = body.findAll('div', id_='toc')
+    for tag in div_toc:
+        tag.decompose()
+
+    table = body.findAll('table', class_='ambox')
+    for tag in table:
+        tag.decompose()
+
+    div_asbox = body.findAll('div', class_='asbox')
+    for tag in div_asbox:
+        tag.decompose()
+
+    span = body.findAll('span', class_='mw-editsection')
+    for tag in span:
+        tag.decompose()
+    
+    for data in body.findAll():
+        print(data.get_text(' '))
+        
     return {}
